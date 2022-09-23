f06eccd10cd9fe150e00f815dcf0d98879b48406
PaulaRoxana <paula_girdea@yahoo.com>
2022-09-23 19:47:57 +0300

    m 23.09

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,13 +7,17 @@ public class Main {
     Greeting greeting = new HelloWorldGreeting();
     greeting.sayHello();
 
-    Greeting greeting2 = new Greeting() {
-      @Override
-      public void sayHello() {
-        System.out.println("Hello World");
-      }
-    };
+//    Greeting greeting2 = new Greeting() {
+//      @Override
+//      public void sayHello() {
+//        System.out.println("Hello World");
+//      }
+//    };
+//    greeting2.sayHello();
+
+    Greeting greeting2 = () -> System.out.println("Hello World");
     greeting2.sayHello();
+
 
   }
 
