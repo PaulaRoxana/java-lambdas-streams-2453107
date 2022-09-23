f6229e01edd717df3d92f12c187bc58b38a9c9fc
PaulaRoxana <paula_girdea@yahoo.com>
2022-09-23 20:58:30 +0300

    m 23.09

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.Random;
+import java.util.function.IntBinaryOperator;
 
 public class Main {
 
@@ -20,14 +21,23 @@ public class Main {
     Greeting greeting3 = () -> System.out.println("Hello World");
     greeting3.sayHello();
 
+    //implementing the method from the Calculator interface using LAMBDAS
+    //no need to create a new class to implement the Calculator interface
     Calculator calculator = (x, y) -> {
       Random random = new Random();
       int randomNumber = random.nextInt(50);
       return x * y + randomNumber;
     };
-
+//calling the above method
     System.out.println(calculator.calculate(1, 2));
 
+    IntBinaryOperator calculator2 = (x, y) -> {
+      Random random = new Random();
+      int randomNumber = random.nextInt(50);
+      return x * y + randomNumber;
+    };
+//calling the above method
+    System.out.println(calculator2.applyAsInt(1, 2));
 
   }
 
